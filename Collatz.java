681888a59f81f004b5d12a435aafed65057e997e
Ori Elkaslasi <ori.elkaslasi@post.runi.ac.il>
2025-11-12 19:49:15 +0200

    Initial commit for Homework 2

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,41 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+	    int seed = Integer.parseInt(args[0]);
+	    String mode = args[1];
+		int currentNum;
+		int count;
+		if (mode.equals("v")) {
+			//verbose mode
+			for (int i = 1; i <= seed; i++) {
+
+				currentNum = i;
+				count = 0;
+				System.out.print(currentNum + " ");
+
+				while (currentNum != 1) {
+					
+					if (currentNum % 2 == 0) { currentNum = (currentNum / 2); }
+					else { currentNum = currentNum * 3 + 1; }
+					System.out.print(currentNum + " ");
+					count++; }
+					System.out.println("(" + (count + 1) + ")");				}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+
+		else if (mode.equals("c")) {
+			//consice mode
+			for (int i = 1; i <= seed; i++) {
+
+				currentNum = i;
+				while (currentNum != 1) {
+					
+					if (currentNum % 2 == 0) { currentNum = (currentNum / 2); }
+					else { currentNum = currentNum * 3 + 1; }
+				 }
+				}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+		else { System.out.println("NOT A REAL MODE. only v or C."); }
+}
 }
